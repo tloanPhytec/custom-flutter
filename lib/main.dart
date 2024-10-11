00e24f9106aeefc27291b832d32475bc62c369eb
True Loan <tloan@phytec.com>
2024-10-11 15:02:24 -0700

    update to add toggle button
    Signed-off-by: True Loan <tloan@phytec.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
- 
+
 void main() {
   runApp(MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,9 +11,37 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text('My Flutter App')),
         body: Center(
-          child: Text('Hello, World!'),
+          child: MyHomePage(),
         ),
       ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isToggled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Hello, World!'), // This remains as your original Hello, World! text
+        SizedBox(height: 20), // Adds some space between the text and the button
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _isToggled = !_isToggled; // Toggle the state
+            });
+          },
+          child: Text(_isToggled ? 'ON' : 'OFF'), // Display the button label
+        ),
+      ],
     );
   }
 }
